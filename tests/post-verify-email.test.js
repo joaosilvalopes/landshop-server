dae227859e609fe0cf92b46164e74bdddd63ed3d
joaosilvalopes jpgdsl@gmail.com <joao.silva.lopes@mindera.com>
2019-06-23 16:35:03 +0100

    feat: POST /verify-email

---
+++ tests/post-verify-email.test.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const app = require('../server');
+const dbUtils = require('../db-utils');
+
+describe('POST /verify-email', () => {
+    beforeAll(async () => {
+        await dbUtils.dropTables();
+        await dbUtils.createTables();
+    });
+
+    it('Should fail if token is invalid', async () => {
+        await request(app)
+            .post('/verify-email')
+            .send({ token: 'someinvalidtoken' })
+            .expect(400);
+    });
+
+    it('Should succeed', async () => {
+        const credentials = {
+            username: 'userTest',
+            email: 'user@mail.com',
+            password: '123456',
+        };
+
+        await request(app)
+            .post('/register')
+            .send(credentials);
+
+        const token = jwt.sign({ username: credentials.username }, process.env.JWT_SECRET);
+
+        await request(app)
+            .post('/verify-email')
+            .send({ token })
+            .expect(200);
+    });
+});
