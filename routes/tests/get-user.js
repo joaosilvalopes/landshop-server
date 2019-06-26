96467409cda4bd83a0dd376d810e1f0e5f5d94b4
João Lopes <jpgdsl@gmail.com>
2019-06-26 23:26:28 +0100

    feat: GET /user

--- routes/tests/get-user.js
+++ routes/tests/get-user.js
@@ -1,10 +1,11 @@
 const request = require('supertest');
 const app = require('../../server');
+const globals = require('./globals');
 
-describe('GET /listings', () => {
+describe('GET /user/:username', () => {
     it('Should respond with JSON', async () => {
         await request(app)
-            .get('/listings')
+            .get(`/user/${globals.user.username}`)
             .expect(200)
             .expect('Content-Type', /json/);
     });
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 
 const routes = [
     'get-listings',
+    'get-user',
     'post-register',
     'post-verify-email',
 ];
