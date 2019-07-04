e4bdaedbf5a058613455a23a780a3ea95a45bcac
João Lopes <jpgdsl@gmail.com>
2019-07-04 21:12:49 +0100

    chore: override envoriment variables on tests

--- jest-globals/setup.js
+++ jest-globals/setup.js
@@ -1,5 +1,18 @@
 /* eslint-disable global-require */
+const nodemailer = require('nodemailer');
 
-module.exports = () => {
+module.exports = async () => {
     require('dotenv').config();
+
+    const testAccount = await nodemailer.createTestAccount();
+
+    // override enviroment variables
+    process.env = {
+        ...process.env,
+        NODEMAILER_HOST: 'smtp.ethereal.email',
+        NODEMAILER_PORT: 587,
+        NODEMAILER_SECURE: '',
+        NODEMAILER_USERNAME: testAccount.user,
+        NODEMAILER_PASSWORD: testAccount.pass,
+    };
 };
